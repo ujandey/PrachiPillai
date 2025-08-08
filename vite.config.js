5daf24980c7ac84f8bc992a8c1f6e3fb74cf18e7
ujandey <ujandey007@gmail.com>
2025-08-08 13:46:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/PrachiPillai/',
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PrachiPillai/',
 })
